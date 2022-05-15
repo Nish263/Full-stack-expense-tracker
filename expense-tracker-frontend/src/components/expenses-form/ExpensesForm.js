48a945235df926bdb45113f16b94530779c1f205
Nisha Paudel Pandey <nshpoudel263@gmail.com>
2022-05-16 09:39:49 +1000

    form data

--- expense-tracker-frontend/src/components/expenses-form/ExpensesForm.js
+++ expense-tracker-frontend/src/components/expenses-form/ExpensesForm.js
@@ -1,40 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Row, Col, Button } from "react-bootstrap";
 
+const initialState = {
+  name: "",
+  amount: "",
+  date: "",
+};
 export const ExpensesForm = () => {
+  const { formData, SetFormData } = useState(initialState);
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+
+    SetFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleOnSubmit = (event) => {
+    event.preventDefault();
+    console.log("submit");
+  };
   return (
     <div>
-      <Row className="g-3 bg-info p-4 rounded ">
-        <Col md="4">
-          <Form.Control
-            type="text"
-            class="form-control"
-            placeholder=" expenses name"
-            required
-          />
-        </Col>
-        <Col MD="2">
-          <Form.Control
-            type="number"
-            class="form-control"
-            placeholder=" amount"
-            required
-          />
-        </Col>
-        <Col md="4">
-          <Form.Control
-            type="date"
-            class="form-control"
-            placeholder=" amount"
-            required
-          />
-        </Col>
-        <Col>
-          <Button variant="primary" type="submit">
-            Add
-          </Button>
-        </Col>
-      </Row>
+      <Form onSubmit={handleOnSubmit}>
+        <Row className="g-3 bg-info p-4 rounded ">
+          <Col md="4">
+            <Form.Control
+              type="text"
+              class="form-control"
+              placeholder=" expenses name"
+              name="name"
+              onChange={handleOnChange}
+              required
+            />
+          </Col>
+          <Col MD="2">
+            <Form.Control
+              type="number"
+              class="form-control"
+              placeholder=" amount"
+              name="amount"
+              onChange={handleOnChange}
+              required
+            />
+          </Col>
+          <Col md="4">
+            <Form.Control
+              type="date"
+              class="form-control"
+              placeholder=" date"
+              name="date"
+              onChange={handleOnChange}
+              required
+            />
+          </Col>
+          <Col>
+            <Button variant="primary" type="submit">
+              Add
+            </Button>
+          </Col>
+        </Row>
+      </Form>
     </div>
   );
 };
